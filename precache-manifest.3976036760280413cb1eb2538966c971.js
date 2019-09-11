6c0b326b5f4c4ee18a62e19fc50649ca446608d0
Kh-Ol-An <olegkhristenko@gmail.com>
2019-09-11 11:45:39 +0300

    Updates

--- precache-manifest.3976036760280413cb1eb2538966c971.js
+++ precache-manifest.3976036760280413cb1eb2538966c971.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d866a1e845f72462cd007f0bb35f9573",
+    "revision": "6d3626f2e9c705d26e7d4dfe076f7867",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
-    "revision": "adda073284ce5dbb3eae",
+    "revision": "6ff1df6ab6585de41d0d",
     "url": "/goit-react-hw-03-image-finder/static/css/main.8d176d4c.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/js/2.a36f212b.chunk.js"
   },
   {
-    "revision": "adda073284ce5dbb3eae",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.d26a7215.chunk.js"
+    "revision": "6ff1df6ab6585de41d0d",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.cfff24ec.chunk.js"
   },
   {
     "revision": "67b3b4e9d25cbd99b2d3",
